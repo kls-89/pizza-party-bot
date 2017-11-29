57a4fa913cdaeb45700c491362a171d6a4ac7d56
Keenan <k.leonardsolis@gmail.com>
2017-11-29 04:33:21 -0500

    Uploaded Pizza Bot config file

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key: 'YOUR-CONSUMER-KEY-HERE',
+    consumer_secret: 'YOUR-SECRET-CONSUMER-KEY-HERE',
+    access_token: 'YOUR-ACCESS-TOKEN-HERE',
+    access_token_secret: 'YOUR-ACCESS-TOKEN-SECRET-HERE',
+    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+}
